ce855f3c874ff1ce691cde48429ce929de0c02cd
MSI\Jim <jim87719@gmail.com>
2023-04-30 22:50:39 +0800

    complete

--- src/Calculate.js
+++ src/Calculate.js
@@ -1,72 +1,84 @@
 import "./Calculate.css";
 import { useState } from "react";
-
-function preOutputComplete(operand) {
-  if (operand == null) return;
-  else {
-    console.log(operand);
-    return operand;
-  }
-}
-
 export const CalculateForm = () => {
-  const [curNum, setcurNum] = useState("0");
-  const [preNum, setpreNum] = useState("0");
+  const ops = ["/", "*", "+", "-", ".", "%"];
+
+  const [calc, setCalc] = useState("");
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    setCalc(calc + value);
+  };
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  const claerNum = () => {
+    setCalc("");
+  };
   return (
     <div className="row container justify-content-center mt-3">
-      <div className="currOutput ">
-        <div className="preOutput">{preOutputComplete(preNum)}</div>
-        {parseInt(curNum)}
-      </div>
-      <div className=" calculator mt-3" onClick={() => setcurNum("0")}>
+      <div className="currOutput ">{calc || "0"}</div>
+      <div className=" numberZero mt-3" onClick={() => claerNum()}>
         AC
       </div>
-      <div className=" calculator mt-3">±</div>
-      <div className=" calculator mt-3">%</div>
-      <div className=" calculator mt-3">÷</div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "7")}>
+      {/* <div className=" calculator mt-3">±</div> */}
+      <div className=" calculator mt-3" onClick={() => updateCalc("%")}>
+        %
+      </div>
+      <div className=" calculator mt-3" style={{color:"rgba(201, 148, 3, 0.747)"}} onClick={() => updateCalc("/")}>
+        ÷
+      </div>
+      <div className=" number mt-3" onClick={() => updateCalc("7")}>
         7
       </div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "8")}>
+      <div className=" number mt-3" onClick={() => updateCalc("8")}>
         8
       </div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "9")}>
+      <div className=" number mt-3" onClick={() => updateCalc("9")}>
         9
       </div>
-      <div className=" calculator mt-3">*</div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "4")}>
+      <div className=" calculator mt-3" style={{color:"rgba(201, 148, 3, 0.747)"}} onClick={() => updateCalc("*")}>
+        *
+      </div>
+      <div className=" number mt-3" onClick={() => updateCalc("4")}>
         4
       </div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "5")}>
+      <div className=" number mt-3" onClick={() => updateCalc("5")}>
         5
       </div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "6")}>
+      <div className=" number mt-3" onClick={() => updateCalc("6")}>
         6
       </div>
-      <div className=" calculator mt-3">-</div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "1")}>
+      <div className=" calculator mt-3" style={{color:"rgba(201, 148, 3, 0.747)"}} onClick={() => updateCalc("-")}>
+        -
+      </div>
+      <div className=" number mt-3" onClick={() => updateCalc("1")}>
         1
       </div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "2")}>
+      <div className=" number mt-3" onClick={() => updateCalc("2")}>
         2
       </div>
-      <div className=" number mt-3" onClick={() => setcurNum(curNum + "3")}>
+      <div className=" number mt-3" onClick={() => updateCalc("3")}>
         3
       </div>
-      <div
-        className=" calculator mt-3"
-        onClick={() => {
-          setcurNum(0);
-          setpreNum(curNum + "+");
-        }}
-      >
+      <div className=" calculator mt-3" style={{color:"rgba(201, 148, 3, 0.747)"}} onClick={() => updateCalc("+")}>
         +
       </div>
-      <div className=" numberZero mt-3" onClick={() => setcurNum(curNum + "0")}>
+      <div className=" numberZero mt-3" onClick={() => updateCalc("0")}>
         0
       </div>
-      <div className=" number mt-3">.</div>
-      <div className=" calculator mt-3">=</div>
+      <div className=" number mt-3" onClick={() => updateCalc(".")}>
+        .
+      </div>
+      <div className=" calculator mt-3" style={{color:"rgba(201, 148, 3, 0.747)"}} onClick={() => calculate()}>
+        =
+      </div>
     </div>
   );
 };
